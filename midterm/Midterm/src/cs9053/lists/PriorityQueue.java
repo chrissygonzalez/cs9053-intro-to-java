406d3bdce6351316798fb5feec3fa93a9bdd4e0a
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2024-07-05 18:12:24 -0400

    Begin refactoring PQ to use ArrayList instead of LinkedList

--- midterm/Midterm/src/cs9053/lists/PriorityQueue.java
+++ midterm/Midterm/src/cs9053/lists/PriorityQueue.java
@@ -1,8 +1,11 @@
 package cs9053.lists;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
-public class PriorityQueue<E extends Comparable<E>> extends LinkedList<E> {
+public class PriorityQueue<E extends Comparable<E>> extends List<E> implements Queue<E> {
+	private ArrayList<E> data = new ArrayList<E>();
 	private Comparator<? super E> comparator = null;
 	
 	public PriorityQueue() {
@@ -20,13 +23,79 @@ public class PriorityQueue<E extends Comparable<E>> extends LinkedList<E> {
     	}
     }
 	
-	@Override
+    // Queue methods
+    // TODO add heapify and call heapify after insert
 	public boolean offer(E e) {
 		int index = this.size() == 0 ? 0 : findAddIndex(e, 0, this.size() - 1);
 		this.add(e, index);
 		return true;
 	}
 	
+	public E remove() throws NoSuchElementException {
+		if(data.size() == 0) {
+			try {
+				throw new NoSuchElementException();
+			} catch(NoSuchElementException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		
+		int lastIndex = data.size() - 1;
+		Collections.swap(data, 0, lastIndex);
+		return data.remove(lastIndex);
+	}
+	
+	public E peek() {
+		if(data.size() == 0) return null;
+		return data.get(0);
+	}
+	
+	public E poll() {
+		if(data.size() == 0) return null;
+		return this.remove();
+	}
+	
+	public E element() throws NoSuchElementException {
+		if(data.size() == 0) {
+			try {
+				throw new NoSuchElementException();
+			} catch(NoSuchElementException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return data.get(0);
+	}
+	
+	// List methods
+	public void add(E e) {
+		this.offer(e);
+	}
+	
+	public E get(int i) {
+		return data.get(i);
+	}
+	
+	public E remove(int i) {
+		return data.remove(i);
+	}
+	
+	public void set(E e, int i) {
+		// not sure what to do here
+	}
+	
+	public void add(E e, int i) {
+		// not sure what to do here
+	}
+	
+	public int size() {
+		return data.size();
+	}
+	
+	public boolean contains(Object o) {
+		return data.contains(o);
+	}
+	
+	// may not need this method anymore
 	private int findAddIndex(E e, int start, int end) {
 		if(start > end) {
 			return start;
